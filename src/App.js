d1f40276459482a394fddb64ea60b25b39e85d61
LiuLi <li.liu@uipath.com>
2021-07-20 21:06:58 +0900

    detect image after clicking

--- src/App.js
+++ src/App.js
@@ -29,17 +29,21 @@ class App extends Component{
     super();
     this.state ={
       input:'',
+      imageUrl:'',
     }
   }
 
   onInputChange =(event)=>{
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
 
   onButtonSubmit =() =>{
-    app.models.predict("18a306b7d8964baeb78ffa886be075e0", this.state.input).then(
+    this.setState({imageUrl:this.state.input});
+    //_Clarifai.FACE_DETECT_MODEL
+    //Clarifai.GENERAL_MODEL
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
     function(response){
-      console.log(response);
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
     },
     function(err){
 
@@ -61,7 +65,7 @@ class App extends Component{
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}
         />
-      <FaceRecognition />
+      <FaceRecognition imageUrl={this.state.imageUrl}/>
 
     </div>
   );
